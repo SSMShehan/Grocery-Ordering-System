a1162c3e9a5b36f8f2a8bb477977289bc5b6e5eb
SSMshehan <shehanmadusanka775@gmail.com>
2025-05-06 00:06:44 +0530

    commit1

---
+++ Grocery_Ordering_System/src/main/java/Grocery_Ordering_System/DBconnection.java
@@ -0,0 +1,26 @@
+package Grocery_Ordering_System;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DBconnection {
+	private static String url = "jdbc:mysql://localhost:3306/grocery_store";
+	private static String user = "root";
+	private static String pass = "Admin@12345";
+	private static Connection con;
+	
+	public static Connection getConnection() {
+		try {
+			// Updated driver class name for newer MySQL versions
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			System.out.println("Attempting to connect to database: " + url);
+			con = DriverManager.getConnection(url, user, pass);
+			System.out.println("Database connection successful");
+		}
+		catch(Exception e) {
+		    System.out.println("Database connection error: " + e.getMessage());
+		    e.printStackTrace();
+		}
+		return con;
+	}
+}
